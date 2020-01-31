1eeac227f274fa78febf4de0442c3758668f0835
Nikunj Beladiya <nikunj.beladiya@tarento.com>
2020-01-31 15:40:47 +0530

    Fix procfile and change app port

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const latexService = require('./latexService.js')
 const cors = require('cors')
 
 const app = express()
+const PORT = process.env.PORT || 3100;
 app.use(cors())
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -13,6 +14,6 @@ app.use(bodyParser.json())
 app.get("/latex/convert", latexService.convert);
 app.post("/latex/convert", bodyParser.json({ limit: '1mb' }), latexService.convert);
 
-app.listen(3100, () => {
+app.listen(PORT, () => {
     console.log("Server is listening on port 3100");
 });
